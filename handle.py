8e02dd1dbf7873e779501666eb22e71622e83691
Steven <ll3573@nyu.edu>
2018-02-18 15:44:39 -0500

    flask demo

--- handle.py
+++ handle.py
@@ -23,7 +23,7 @@ class Handle(object):
             sha1 = hashlib.sha1()
             map(sha1.update, list)
             hashcode = sha1.hexdigest()
-            print "handle/GET func: hashcode, signature: ", hashcode, signature
+            print("handle/GET func: hashcode, signature: ", hashcode, signature)
             if hashcode == signature:
                 return echostr
             else:
@@ -34,7 +34,7 @@ class Handle(object):
     def POST(self):
         try:
             webData = web.data()
-            print "Handle Post webdata is ", webData
+            print("Handle Post webdata is ", webData)
             # 后台打日志
             recMsg = receive.parse_xml(webData)
             if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
@@ -44,7 +44,7 @@ class Handle(object):
                 replyMsg = reply.TextMsg(toUser, fromUser, content)
                 return replyMsg.send()
             else:
-                print "暂且不处理"
+                print("暂且不处理")
                 return "success"
         except Exception, Argment:
             return Argment
